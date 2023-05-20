7a58591619dd6e10833a9b7d3eb18da70877c36a
@hexlet/cli <support@hexlet.io>
2023-05-20 21:04:48 +0600

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -8,6 +8,9 @@ public class App {
     public static HashMap<String, Integer> getWordCount(String text) {
         HashMap<String, Integer> hm = new HashMap<>();
         String[] arrText = text.split(" ");
+        if (arrText.length == 0) {
+            return hm;
+        }
         for (String word : arrText) {
             int count = 0;
             for (String wordPair : arrText) {
@@ -21,8 +24,22 @@ public class App {
     }
 
     public static String toString(Map<String, Integer> map) {
-        return map.toString().replace("{", "  ").replace("}", "")
-                .replace("=", ": ").replace(",", "\n ");
+        if (map.isEmpty()){
+            return "{}";
+        }
+        StringBuilder finalStr = new StringBuilder("{\n");
+        for (Map.Entry<String, Integer> maps : map.entrySet()) {
+            finalStr.append("  " + maps.getKey() + ": ").append(maps.getValue() + "\n");
+        }
+        finalStr.append("}");
+        return finalStr.toString();
+    }
+
+    public static void main (String[] args) {
+        var text = "the java is the best programming language java";
+        var r1 = getWordCount(text);
+        var r2 = toString(r1);
+        System.out.println(r2);
     }
 }
 //END
