db3efe6f7f6c68d23082be94cd5cc2567bbd032a
@hexlet/cli <support@hexlet.io>
2023-05-20 20:37:46 +0600

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -4,22 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 // BEGIN
-public class App{
+public class App {
     public static HashMap<String, Integer> getWordCount(String text) {
         HashMap<String, Integer> hm = new HashMap<>();
         String[] arrText = text.split(" ");
         for (String word : arrText) {
             int count = 0;
             for (String wordPair : arrText) {
-                if(word.equalsIgnoreCase(wordPair)) {
-                    count+=1;
+                if (word.equalsIgnoreCase(wordPair)) {
+                    count += 1;
                 }
             }
             hm.put(word, count);
         }
         return hm;
     }
-    public static String toString (Map<String, Integer> map) {
+
+    public static String toString(Map<String, Integer> map) {
         return map.toString().replace("{", "  ").replace("}", "")
                 .replace("=", ": ").replace(",", "\n ");
     }
