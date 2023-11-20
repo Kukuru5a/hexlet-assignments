2d9528231b9fe0913390d657fba2eb33e04798e5
@hexlet/cli <support@hexlet.io>
2023-11-20 19:07:38 +0600

    submit java-spring-ru/jpa-data

---
+++ java-spring-ru/jpa-data/src/main/java/exercise/controller/PeopleController.java
@@ -0,0 +1,49 @@
+package exercise.controller;
+
+import exercise.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+
+//import java.lang.foreign.Linker;
+import java.util.List;
+import java.util.Optional;
+
+import exercise.model.Person;
+
+@RestController
+@RequestMapping("/people")
+public class PeopleController {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @GetMapping(path = "/{id}")
+    public Person show(@PathVariable long id) {
+        return personRepository.findById(id).get();
+    }
+
+    // BEGIN
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Person> index(@RequestParam(defaultValue = "10") Integer limit) {
+//        return personRepository.findAll().stream().limit(limit).toList();
+        return personRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Person create (@Validated @RequestBody Person p) {
+        var res = personRepository.save(p);
+        return res;
+    }
+
+    @DeleteMapping("{id}")
+    public void destroy(@PathVariable Long id) {
+        var user = personRepository.findById(id).get();
+        personRepository.delete(user);
+    }
+    // END
+}
---
+++ java-spring-ru/jpa-data/src/main/java/exercise/model/Person.java
@@ -0,0 +1,26 @@
+package exercise.model;
+
+import jakarta.persistence.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+// BEGIN
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Person {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column
+    private String firstName;
+    private String lastName;
+}
+// END
