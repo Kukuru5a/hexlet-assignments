5c199e6426821b39c7224f33140ce4f97b2247ae
@hexlet/cli <support@hexlet.io>
2023-11-20 22:16:12 +0600

    submit java-spring-ru/error-handling

---
+++ java-spring-ru/error-handling/src/main/java/exercise/controller/ProductsController.java
@@ -0,0 +1,57 @@
+package exercise.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.Optional;
+
+import exercise.model.Product;
+import exercise.repository.ProductRepository;
+import exercise.exception.ResourceNotFoundException;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping(path = "")
+    public List<Product> index() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product create(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    // BEGIN
+    @PutMapping("/{id}")
+    public Product update(@PathVariable String id, @RequestBody Product product) {
+        var good = productRepository.findById(Long.valueOf(id))
+                .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+        good.setTitle(product.getTitle());
+        good.setPrice(product.getPrice());
+        productRepository.save(good);
+        return good;
+
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Product show(@PathVariable String id) {
+        var product = productRepository.findById(Long.valueOf(id))
+                .orElseThrow(()->new ResourceNotFoundException("Product with id " + id + " not found"));
+        return product;
+    }
+    // END
+
+    @DeleteMapping(path = "/{id}")
+    public void delete(@PathVariable long id) {
+        productRepository.deleteById(id);
+    }
+}
